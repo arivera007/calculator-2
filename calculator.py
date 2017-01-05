632170c70fb8b2d1ac712da5ba3ee236d7aa8cd3
Hackbright Student <no-reply@hackbrightacademy.com>
2017-01-04 17:16:04 -0800

    not robust but function validation added

--- calculator.py
+++ calculator.py
@@ -8,28 +8,34 @@ calculator program yourself in this file.
 from arithmetic import *
 
 
+def is_input_valid(token_list):
+    return True
+
 # Your code goes here
 while True:
     user_input = raw_input("prefix equation: >  ")
-    token_list = user_input.split(" ")
+    token_list = user_input.split(' ')
     if token_list[0] == 'q':
         break
     else:
-        if token_list[0] == '+':  # Culturally using double quotes is used for messages
-            print add(int(token_list[1]), int(token_list[2]))
-        elif token_list[0] == "-":
-            print subtract(int(token_list[1]), int(token_list[2]))
-        elif token_list[0] == "*":
-            print multiply(int(token_list[1]), int(token_list[2]))
-        elif token_list[0] == "/":
-            print divide(int(token_list[1]), int(token_list[2]))
-        elif token_list[0] == "square":
-            print square(int(token_list[1]))
-        elif token_list[0] == "cube":
-            print cube(int(token_list[1]))
-        elif token_list[0] == "power":
-            print power(int(token_list[1]), int(token_list[2]))
-        elif token_list[0] == "mod":
-            print mod(int(token_list[1]), int(token_list[2]))
+        if is_input_valid(token_list):
+            if token_list[0] == '+':
+                print add(int(token_list[1]), int(token_list[2]))
+            elif token_list[0] == '-':
+                print subtract(int(token_list[1]), int(token_list[2]))
+            elif token_list[0] == '*':
+                print multiply(int(token_list[1]), int(token_list[2]))
+            elif token_list[0] == '/':
+                print divide(int(token_list[1]), int(token_list[2]))
+            elif token_list[0] == 'square':
+                print square(int(token_list[1]))
+            elif token_list[0] == 'cube':
+                print cube(int(token_list[1]))
+            elif token_list[0] == 'power':
+                print power(int(token_list[1]), int(token_list[2]))
+            elif token_list[0] == 'mod':
+                print mod(int(token_list[1]), int(token_list[2]))
+            else:
+                print token_list[0] + " is not a valid operation. Try again."
         else:
-            print token_list[0] + " is not a valid operation. Try again."
+            print "That is not a valid input"
